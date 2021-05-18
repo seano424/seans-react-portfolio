e875308e7d850092e7f8e21a9178cb06727122b3
Sean OReilly <soreilly424@gmail.com>
2021-05-18 11:48:50 -0500

    removed purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: [],
   darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {

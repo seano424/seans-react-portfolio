a40e73ab7d7d0b81127cbf32c4895fa689032341
Sean OReilly <soreilly424@gmail.com>
2021-05-21 21:54:19 -0500

    removed app head

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,9 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
-import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
-      <Head>
-        <link rel="shortcut icon" href="/images/favicon.ico" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/images/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/images/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/images/favicon-16x16.png"
-        />
-      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   );

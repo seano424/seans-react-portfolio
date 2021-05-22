35f53073b15be05736a4c1cbe86f144c6f0e004e
Sean OReilly <soreilly424@gmail.com>
2021-05-21 21:44:09 -0500

    added favicon path

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,6 @@ import React from "react";
 import Footer from "./Footer";
 import styles from "../styles/Home.module.css";
 import Navbar from "./Navbar";
-import Head from "next/head";
 import Meta from "./Meta";
 
 export default function Layout({ children }) {

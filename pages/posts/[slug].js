0e098624a04edb6ddafd23e8f8b82986b10fe943
Sean OReilly <soreilly424@gmail.com>
2021-06-07 21:05:41 -0500

    added the NEXT_PUBLIC to env

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,42 +1,42 @@
-import React from "react";
-import { NotionRenderer } from "react-notion";
-import Layout from "../../components/Layout";
-import { getAllPosts } from "./";
+import React from 'react'
+import { NotionRenderer } from 'react-notion'
+import Layout from '../../components/Layout'
+import { getAllPosts } from './'
 
 export default function Post({ post, slug, blocks }) {
-  console.log(post, slug);
-  console.log(blocks);
-  return (
-    <Layout>
-      <section className="w-7/12 mt-10 dark:bg-gray-200 rounded p-5">
-        <NotionRenderer blockMap={blocks} />
-      </section>
-    </Layout>
-  );
+    console.log(post, slug)
+    console.log(blocks)
+    return (
+        <Layout>
+            <section className="w-7/12 mt-10 dark:bg-gray-200 rounded p-5">
+                <NotionRenderer blockMap={blocks} />
+            </section>
+        </Layout>
+    )
 }
 
 export async function getStaticProps({ params: { slug } }) {
-  const posts = await getAllPosts();
-  const post = posts.find((t) => t.slug === slug);
-  // const id = post.id
-  // https://notion-api.splitbee.io/v1/page/5f37eb22-d15d-4551-b87e-9251ceb3cb71
-  const blocks = await fetch(
-    `https://notion-api.splitbee.io/v1/page/${post.id}`
-  ).then((res) => res.json());
+    const posts = await getAllPosts()
+    const post = posts.find((t) => t.slug === slug)
+    // const id = post.id
+    // https://notion-api.splitbee.io/v1/page/5f37eb22-d15d-4551-b87e-9251ceb3cb71
+    const blocks = await fetch(
+        `https://notion-api.splitbee.io/v1/page/${post.id}`
+    ).then((res) => res.json())
 
-  return {
-    props: {
-      post,
-      slug,
-      blocks,
-    },
-  };
+    return {
+        props: {
+            post,
+            slug,
+            blocks,
+        },
+    }
 }
 
 export async function getStaticPaths() {
-  const posts = await getAllPosts(process.env.NOTION_ID);
-  return {
-    paths: posts.map((row) => `/posts/${row.slug}`),
-    fallback: false,
-  };
+    const posts = await getAllPosts(process.env.NEXT_PUBLIC_NOTION_ID)
+    return {
+        paths: posts.map((row) => `/posts/${row.slug}`),
+        fallback: false,
+    }
 }

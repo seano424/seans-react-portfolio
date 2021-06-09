4f7d8c84a2f0cb71fe6b5bf294a1374766b89335
Sean OReilly <soreilly424@gmail.com>
2021-06-09 18:41:51 -0500

    added a contact page...

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,23 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class", // or 'media' or 'class'
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
       width: {
-        "1/7": "14.2857143%",
-        "2/7": "28.5714286%",
-        "3/7": "45%",
-        "4/7": "57.1428571%",
-        "5/7": "71.4285714%",
-        "6/7": "85.7142857%",
+        '1/7': '14.2857143%',
+        '2/7': '28.5714286%',
+        '3/7': '45%',
+        '4/7': '57.1428571%',
+        '5/7': '71.4285714%',
+        '6/7': '85.7142857%',
+        101: '27rem',
       },
       colors: {
-        primary: "#FF017E",
-        dark: "#111927",
+        primary: '#FF017E',
+        dark: '#111927',
       },
       animation: {
-        "pulse-slow": "pulse 10s linear infinite",
+        'pulse-slow': 'pulse 10s linear infinite',
       },
     },
   },
@@ -24,4 +25,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-};
+}

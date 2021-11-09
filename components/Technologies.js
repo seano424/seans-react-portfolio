a2309fbdde5b17b36c8b4e9d0d32b65ddbbafda0
Sean OReilly <soreilly424@gmail.com>
2021-11-09 10:27:28 -0500

    cleaned up tech section

--- components/Technologies.js
+++ components/Technologies.js
@@ -1,61 +1,15 @@
-import {
-  SiNextDotJs,
-  SiReact,
-  SiRails,
-  SiJavascript,
-  SiRuby,
-  SiTailwindcss,
-  SiSass,
-  SiHeroku,
-  SiPostgresql,
-  SiRedux,
-} from 'react-icons/si'
-const technologies = [
-  {
-    icon: <SiNextDotJs />,
-    name: 'NextJS',
-  },
-  {
-    icon: <SiTailwindcss />,
-    name: 'TailwindCSS',
-  },
-  {
-    icon: <SiReact />,
-    name: 'React',
-  },
-  {
-    icon: <SiRedux />,
-    name: 'Redux',
-  },
-  {
-    icon: <SiRails />,
-    name: 'Ruby on Rails',
-  },
-  {
-    icon: <SiJavascript />,
-    name: 'JavaScript',
-  },
-  {
-    icon: <SiPostgresql />,
-    name: 'PostgreSQL',
-  },
-]
+import styles from '@/styles/Technologies.module.css'
+import { technologies } from '@/lib/technologies'
 
 export default function Technologies() {
   return (
-    <div
-      name="technologies"
-      className="flex flex-col my-10 justify-center items-center"
-    >
-      <h3 className="font-black text-center mb-8 md:mb-16 dark:text-white text-black text-3xl md:text-5xl bg-secondary bg-opacity-20 py-2 px-2">
+    <div name="technologies" className={styles.container}>
+      <h3 className={`${styles.header} dark:text-white`}>
         Technologies I frequently use...
       </h3>
-      <div className="flex flex-col md:flex-row flex-wrap gap-10 font-mono justify-center items-center w-10/12">
+      <div className={styles.list}>
         {technologies.map((item) => (
-          <div
-            key={item.name}
-            className="flex justify-center items-center gap-2 text-xl md:text-3xl"
-          >
+          <div key={item.name} className={styles.item}>
             <span className="md:text-4xl">{item.icon}</span>{' '}
             <span>{item.name}</span>
           </div>
